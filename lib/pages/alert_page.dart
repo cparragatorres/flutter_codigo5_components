72a6cc4c364e65f8827fcec97620ccebca999683
cparragatorres <humbertpt14@gmail.com>
2022-05-29 06:33:54 -0500

    Alert Page 4

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -3,12 +3,129 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AlertPage extends StatelessWidget {
+  void showMyAlert2(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Caregiver Review",
+                style: GoogleFonts.poppins(
+                  color: Color(0xff658bc9),
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Divider(),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                padding: const EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  color: Color(0xff789bd6),
+                  borderRadius: BorderRadius.circular(100.0),
+                ),
+                child: Icon(
+                  Icons.person,
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              Text(
+                "Amanda Johnson",
+                style: GoogleFonts.poppins(
+                  color: Color(0xff53658c),
+                  fontWeight: FontWeight.w600,
+                  fontSize: 14.0,
+                ),
+              ),
+              Text(
+                "Rate the care provider Sunday, Jan 9",
+                style: GoogleFonts.poppins(
+                  color: Color(0xff53658c),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 10.0,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.star, color: Color(0xffffbc6b)),
+                  Icon(Icons.star, color: Color(0xffffbc6b)),
+                  Icon(Icons.star, color: Color(0xffffbc6b)),
+                  Icon(Icons.star, color: Color(0xffffbc6b)),
+                  Icon(Icons.star, color: Color(0xffdfe4ed)),
+                ],
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+                height: 50.0,
+                width: double.infinity,
+                color: Color(0xfff7f9fc),
+                child: Text(
+                  "Additional Comments...",
+                  style: GoogleFonts.poppins(
+                    color: Color(0xff949f89),
+                    fontSize: 11.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Hot Now",
+                      style: GoogleFonts.poppins(
+                        color: Color(0xff5f7caf),
+                        fontWeight: FontWeight.w600,
+                        fontSize: 13.0,
+                      ),
+                    ),
+                  ),
+                  MaterialButton(
+                    color: Color(0xff6f8fc5),
+                    child: Text(
+                      "Submint Review",
+                      style: GoogleFonts.poppins(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 13.0,
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   void showMyAlert(BuildContext context) {
     showDialog(
       context: context,
-      builder: (BuildContext context){
+      builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("My Alert Dialog"),
+          // title: Text("My Alert Dialog"),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(20.0),
           ),
@@ -31,22 +148,20 @@ class AlertPage extends StatelessWidget {
                 ),
               ),
               Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-                style: GoogleFonts.poppins(
-                  fontSize: 13.0,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.black.withOpacity(0.5),
-                )
-              ),
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                  style: GoogleFonts.poppins(
+                    fontSize: 13.0,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black.withOpacity(0.5),
+                  )),
             ],
           ),
           actions: [
             ElevatedButton(
-                onPressed: (){
-                  Navigator.pop(context);
-                },
-                child:
-                  Text("Aceptar"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Aceptar"),
             ),
           ],
         );
@@ -68,7 +183,8 @@ class AlertPage extends StatelessWidget {
           children: [
             ElevatedButton(
               onPressed: () {
-                showMyAlert(mandarina);
+                //showMyAlert(mandarina);
+                showMyAlert2(mandarina);
               },
               child: Text(
                 "Show Alert",
