82a7d31c53e6b82a98f3c7e3ae72870703ab11c7
cparragatorres <humbertpt14@gmail.com>
2022-05-29 04:19:36 -0500

    Alert Page 3

--- lib/pages/alert_page.dart
+++ lib/pages/alert_page.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AlertPage extends StatelessWidget {
-  void showMyAlert(BuildContext matasquita) {
+  void showMyAlert(BuildContext context) {
     showDialog(
-      context: matasquita,
-      builder: (BuildContext federico){
-        return Text("Hola");
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text("My Alert Dialog"),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network(
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Check_green_icon.svg/2048px-Check_green_icon.svg.png",
+                height: 90.0,
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "Flutter Alert",
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+                style: GoogleFonts.poppins(
+                  fontSize: 13.0,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.black.withOpacity(0.5),
+                )
+              ),
+            ],
+          ),
+          actions: [
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                child:
+                  Text("Aceptar"),
+            ),
+          ],
+        );
       },
     );
   }
