6f763479b317e8f271176dc6d809667266cc4671
cparragatorres <humbertpt14@gmail.com>
2022-05-30 04:53:41 -0500

    Input Page 3.1

--- lib/pages/input_pages.dart
+++ lib/pages/input_pages.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class InputPage extends StatelessWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -22,8 +23,7 @@ class InputPage extends StatelessWidget {
                 textAlign: TextAlign.start,
                 maxLength: 20,
                 cursorColor: Colors.deepPurpleAccent,
-                style:
-                const TextStyle(
+                style: const TextStyle(
                   color: Colors.deepPurpleAccent,
                   fontSize: 18.0,
                   fontWeight: FontWeight.bold,
@@ -38,54 +38,54 @@ class InputPage extends StatelessWidget {
                   ),
                   icon: Icon(Icons.email),
                   prefixIcon: Icon(Icons.alternate_email),
-                  suffixIcon: Icon(Icons.email_outlined,color: Colors.purpleAccent),
+                  suffixIcon:
+                      Icon(Icons.email_outlined, color: Colors.purpleAccent),
                 ),
-                onChanged: (String value){
+                onChanged: (String value) {
                   print(value);
                 },
               ),
-              const SizedBox(height: 20.0),
               const TextField(
                 decoration: InputDecoration(
-                  hintText: "Buscar producto",
-                  prefixIcon: Icon(Icons.search),
-                  enabled: true,
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.redAccent,
-                      width: 4.0,
+                    hintText: "Buscar producto",
+                    prefixIcon: Icon(Icons.search),
+                    enabled: true,
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.redAccent,
+                        width: 4.0,
+                      ),
                     ),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.yellowAccent,
-                      width: 4.0,
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.yellowAccent,
+                        width: 4.0,
+                      ),
                     ),
-                  ),
-                  disabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.green,
-                      width: 4.0,
-                    ),
-                  )
-                ),
+                    disabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.green,
+                        width: 4.0,
+                      ),
+                    )),
               ),
               const SizedBox(height: 30.0),
               TextField(
                 decoration: InputDecoration(
                   icon: Icon(Icons.search),
-                  suffixIcon: Icon(Icons.check_circle_outline),
-                  label: Text("Buscar"),
+                  suffixIcon: Icon(Icons.mail),
+                  prefixIcon: Icon(Icons.check_circle_outline_outlined),
+                  label: const Text("Buscar"),
                   hintText: "Ingresa el producto a buscar...",
                   enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
+                    borderSide: const BorderSide(
                       width: 2.0,
                       color: Colors.deepPurpleAccent,
                     ),
                     borderRadius: BorderRadius.circular(20.0),
                   ),
                   focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
+                    borderSide: const BorderSide(
                       width: 2.0,
                       color: Colors.deepPurpleAccent,
                     ),
@@ -93,6 +93,74 @@ class InputPage extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(height: 30.0),
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.black.withOpacity(0.06),
+                        blurRadius: 12,
+                        offset: const Offset(4, 4)),
+                  ],
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: "Buscar producto...",
+                    hintStyle: GoogleFonts.poppins(
+                      fontSize: 14.0,
+                      color: Colors.black.withOpacity(0.37),
+                    ),
+                    suffixIcon: Container(
+                      margin: EdgeInsets.all(3.0),
+                      decoration: BoxDecoration(
+                        color: Colors.pinkAccent,
+                        borderRadius: BorderRadius.circular(16.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.pink.withOpacity(0.4),
+                            offset: const Offset(4, 4),
+                            blurRadius: 7.0,
+                          ),
+                        ],
+                      ),
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      borderSide: BorderSide(
+                        color: Colors.white,
+                        width: 0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 30.0),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "ingrese su contraseña",
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.remove_red_eye),
+                    onPressed: (){
+                      print("object");
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 100.0),
             ],
           ),
         ),
