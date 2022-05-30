e0129a1bf92433a27b389bb71a6394be9d56c0fc
cparragatorres <humbertpt14@gmail.com>
2022-05-30 03:50:59 -0500

    Input Page 2

--- lib/pages/input_pages.dart
+++ lib/pages/input_pages.dart
@@ -6,6 +6,41 @@ class InputPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("InputPage"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                maxLines: 1,
+                textAlign: TextAlign.start,
+                maxLength: 20,
+                cursorColor: Colors.deepPurpleAccent,
+                style: TextStyle(
+                  color: Colors.deepPurpleAccent,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                decoration: InputDecoration(
+                  label: Text("correo electrónico"),
+                  hintText: "example@gmail.com",
+                  hintStyle: TextStyle(
+                    color: Colors.pinkAccent.withOpacity(0.35),
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
